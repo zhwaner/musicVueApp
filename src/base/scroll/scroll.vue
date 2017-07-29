a126d18a1303a3cacd5ba801170d268d7d6a42d8
zhwaner <392125986@qq.com>
2017-07-29 17:45:32 +0800

    scroll

--- src/base/scroll/scroll.vue
+++ src/base/scroll/scroll.vue
@@ -5,56 +5,55 @@
 </template>
 
 <script>
-	import BScroll from 'better-scroll'
-
-	export default {
-		props: {
-			probeType: {
-				type: Number,
-				default: 1
-			},
-			click: {
-				type: Boolean,
-				default: true
-			},
-			data: {
-				type: Array,
-				default: null
-			}
-		},
-		mounted() {
-			setTimeout(() => {
-				this._initScroll()
-			}, 20)
-		},
-		methods: {
-			_initScroll() {
-				if (!this.$refs.wrapper) {
-					return
-				}
-				this.scroll = new BScroll(this.$refs.wrapper, {
-					probeType: this.probeType,
-					click: this.click
-				})
-			},
-			enabled() {
-				this.scroll && this.scroll.enabled()
-			},
-			disabled() {
-				this.scroll && this.scroll.disabled()
-			},
-			refresh() {
-				this.scroll && this.scroll.refresh()
-			}
-		},
-		watch: {
-			data() {
-				setTimeout(() => {
-					this.refresh()
-				}, 20)
-			}
-		}
-	}
+  import BScroll from 'better-scroll'
+  export default {
+    props: {
+      probeType: {
+        type: Number,
+        default: 1
+      },
+      click: {
+        type: Boolean,
+        default: true
+      },
+      data: {
+        type: Array,
+        default: null
+      }
+    },
+    mounted() {
+      setTimeout(() => {
+        this._initScroll()
+      }, 20)
+    },
+    methods: {
+      _initScroll() {
+        if (!this.$refs.wrapper) {
+          return
+        }
+        this.scroll = new BScroll(this.$refs.wrapper, {
+          probeType: this.probeType,
+          click: this.click
+        })
+      },
+      enabled() {
+        this.scroll && this.scroll.enabled()
+      },
+      disabled() {
+        this.scroll && this.scroll.disabled()
+      },
+      refresh() {
+        this.scroll && this.scroll.refresh()
+      }
+    },
+    watch: {
+      data() {
+        setTimeout(() => {
+          this.refresh()
+        }, 20)
+      }
+    }
+  }
 </script>
 
 <style scoped lang="stylus">

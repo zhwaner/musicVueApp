3e41814b6c38cffa1130309f707d253fc14335ed
zhwaner <392125986@qq.com>
2017-08-06 22:45:22 +0800

    vuex

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,1 +1,2 @@
 // 获取state做一些映射
+export const singer = state => state.singer
--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,1 +1,9 @@
 import * as types from './mutation-types'
+
+const mutations = {
+  [types.SET_SINGER] (state, singer) { // 方法名
+    state.singer = singer
+  }
+}
+
+export default mutations
